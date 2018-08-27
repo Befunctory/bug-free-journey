b16a8f98514a86084a11957c36cafc8d805d6a5c
Befunctory <42401063+Befunctory@users.noreply.github.com>
2018-08-27 13:50:54 +0800

    定时器 cron表达式

--- TestProject/src/main/java/com/lx/quartz/Test2.java
+++ TestProject/src/main/java/com/lx/quartz/Test2.java
@@ -1,27 +1,26 @@
 package com.lx.quartz;
 
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.quartz.impl.StdSchedulerFactory;
-//what meanning?
-import static org.quartz.TriggerBuilder.newTrigger;
-
-import org.quartz.JobDetail;
-
-import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
-import static org.quartz.DateBuilder.newDate;
 import static org.quartz.JobBuilder.newJob;
 
-public class Test1 {
+import org.quartz.CronScheduleBuilder;
+import org.quartz.CronTrigger;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class Test2 {
 	public static void main(String[] args) {
 		try {
 			// 创建 scheduler----->调度器
 			Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 			// 定义一个Trigger----->定义触发条件
-			Trigger trigger = newTrigger().withIdentity("trigger1", "group1").startNow()
+			CronTrigger trigger = (CronTrigger) TriggerBuilder.newTrigger().withIdentity("trigger1", "group1")
 					// 一旦加入schedule，立即生效，使用simpleSchedule,每隔一秒执行，永远执行下去
-					.withSchedule(simpleSchedule().withIntervalInHours(1).repeatForever()).build();
+					// simpleSchedule().withIntervalInHours(1).repeatForever()
+					//每秒钟执行一次，如果需要特定业务的cron表达式，搜索百度生成
+					.withSchedule(CronScheduleBuilder.cronSchedule("0/1 * * * * ? ")).build();
 			// 任务细节------>具体的业务逻辑 每次执行都创建一个新的任务实例
 			JobDetail jobDetail = newJob(HelloQuartz.class).withIdentity("job1", "group1")
 					.usingJobData("name", "quartz").build();
